2f7e0e6b4542b0083e5da4690727ebb23386b274
Jonas Braun <jonas-braun7@web.de>
2021-09-07 12:49:41 +0200

    improved formatting

--- scripts/search_max_com_offset_210506.py
+++ scripts/search_max_com_offset_210506.py
@@ -6,7 +6,7 @@ SCRIPT_PATH, _ = os.path.split(FILE_PATH)
 MODULE_PATH, _ = os.path.split(SCRIPT_PATH)
 sys.path.append(MODULE_PATH)
 
-from longterm import load, utils
+from longterm import load
 
 fly_dirs = [os.path.join(load.NAS_DIR_JB, "210301_J1xCI9", "Fly1"),
             os.path.join(load.LOCAL_DATA_DIR_LONGTERM, "210212", "Fly1"),
@@ -37,4 +37,3 @@ for i_fly, (fly_dir, trial_dirs) in enumerate(zip(fly_dirs, all_trial_dirs)):
             # print(diff)
             print(trial_dir)
     diffs.append(diffs_fly)
-pass
