dd6d1d9603dd91934ba2884cc47f05c16d215b6a
Jonas Braun <jonas-braun7@web.de>
2021-10-13 14:51:46 +0200

    adding docstrings

--- longterm/behaviour/__init__.py
+++ longterm/behaviour/__init__.py
@@ -1,4 +1,4 @@
-    """
-    sub-module for behavioural data pre-processing.
-    Includes pose estimation, ball tracking analysis, and synchronisation of behavioural data with imaging data.
-    """
+"""
+sub-module for behavioural data pre-processing.
+Includes pose estimation, ball tracking analysis, and synchronisation of behavioural data with imaging data.
+"""
