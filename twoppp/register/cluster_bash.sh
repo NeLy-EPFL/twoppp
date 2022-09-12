a070e91cd20764135a5b834613888393f5f057bf
Jonas Braun <jonas-braun7@web.de>
2022-09-12 15:04:04 +0200

    updated NAS mounting based on newly defined lab standards

--- twoppp/register/cluster_bash.sh
+++ twoppp/register/cluster_bash.sh
@@ -2,23 +2,10 @@
 time="50:00:00"
 partition="debug"  # "parallel"
 output="./outputs/slurm-%j.out"
-    
-convert_to_fidis_dir () {
-    fidis_dir=${1//mnt\/NAS\/JB/scratch\/jbraun}
-    fidis_dir=${1//mnt\/NAS2\/JB/scratch\/jbraun}
-    fidis_dir=${1//mnt\/NAS\/LH/scratch\/jbraun}
-    fidis_dir=${1//mnt\/NAS2\/LH/scratch\/jbraun}
-    fidis_dir=${1//mnt\/data\/JB/scratch\/jbraun}
-    fidis_dir=${1//mnt\/data2\/JB/scratch\/jbraun}
-    fidis_dir=${1//mnt\/data\/LH/scratch\/jbraun}
-    fidis_dir=${1//mnt\/data2\/LH/scratch\/jbraun}
-    echo ${fidis_dir}
-}
 
 # while read dir; do
 for dir in $(find /scratch/jbraun/**/**/**/processed -type d); do
     echo ${dir}
-    # folder=$(convert_to_fidis_dir ${dir})
     folder=${dir} 
     if [[ -e ${folder} ]] && [[ ! -f "${folder}/red_com_warped.tif" ]] && [[ ! -f "${folder}/w.npy" ]]; then
         ref_frame="${folder}/../../processed/ref_frame_com.tif"
